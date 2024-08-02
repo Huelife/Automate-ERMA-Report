d70ce6590b1bebb359cacf5ba4b408c3ec258267
Eric Meas <emmeas@gmail.com>
2024-08-02 07:34:19 -0700

    Update ERMA_1.py
    Edited code to be more concise with beginning actions and sleep times

--- ERMA_1.py
+++ ERMA_1.py
@@ -23,18 +23,17 @@ for url in [link]:
     webbrowser.get('chrome').open_new_tab(link) #open link if no errors
 
 #Beginning actions
-pyautogui.moveTo(1700,200)
-pyautogui.click()
 time.sleep(1)          #Sleep for 1 second
-target_main = pyautogui.locateCenterOnScreen("Capture_Main.png",confidence=0.85)
+target_main = pyautogui.locateCenterOnScreen("Capture_20.png",confidence=0.85)
 pyautogui.click(target_main)     #Move the mouse to target_main coordinates and click
+time.sleep(3)          #Sleep for 3 seconds
 
 #Relog to access Tracking Report
 try:
     target_1 = pyautogui.locateCenterOnScreen("Capture_1.png",confidence=0.85)
     pyautogui.click(target_1)     #Move the mouse to target_1 coordinates and click
 
-    time.sleep(1)          #Sleep for 1 second
+    time.sleep(2)          #Sleep for 2 seconds
     target_2 = pyautogui.locateCenterOnScreen("Capture_2.png",confidence=0.85)
     pyautogui.click(target_2)     #Move the mouse to target_2 coordinates and click
 
@@ -42,7 +41,7 @@ try:
     target_3 = pyautogui.locateCenterOnScreen("Capture_3.png",confidence=0.85)
     pyautogui.click(target_3)     #Move the mouse to target_3 coordinates and click
 
-    time.sleep(8)          #Sleep for 8 second
+    time.sleep(9)          #Sleep for 9 seconds
     target_4 = pyautogui.locateCenterOnScreen("Capture_4.png",confidence=0.85)
     pyautogui.click(target_4)     #Move the mouse to target_4 coordinates and click
 
